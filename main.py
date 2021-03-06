ebc7c3f1c131b4680478a680996b720c18651635
swami535misal <68000322+swami535misal@users.noreply.github.com>
2021-03-06 19:52:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 17:49:00 2021
+
+@author: Swami Misal
+"""
+
+from typing import Optional
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get('/')
+def read_root():
+    return {'Hellow':'World'}
+
+@app.get('/items/{item_id}')
+def read_item(item_id:int,q:Optional[str]=None):
+    return {'item_id':item_id,'q':q}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='128.0.0.1', port=8000)
